06a62da9765b50d965828e6ed0b13dc57f862b4c
greg.woffindin@aspencapital.com <36450157+gregjwoffindin@users.noreply.github.com>
2024-04-09 10:45:13 -0700

    Initial commit

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -1,6 +1,6 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 
-export class Inventory {
+export class InventoryPage {
   readonly page: Page;
   readonly inventoryUrl: string = 'https://www.saucedemo.com/inventory.html';
 
--- tests/Login.test.ts
+++ tests/Login.test.ts
@@ -1,13 +1,16 @@
 import { chromium, test, expect} from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
-import { link } from "fs";
+import { InventoryPage } from "../pages/InventoryPage";
 
 test("Login Test", async ({ page }) => {
 
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage();
 
   await loginPage.visit();
+
+  await expect(inventoryPage.inventoryUrl).toBeTruthy()
   
-});
+})
